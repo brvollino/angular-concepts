d2b50b788cbd0c0c71723829406bd9e88ad8e96e
Bruno Vollino <bruno.vollino@ilegra.com>
2019-08-14 15:16:07 -0300

    Tema 3: http user service

--- packages/3 - angular-services/src/app/app.component.ts
+++ packages/3 - angular-services/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { UserService } from './services/user.service';
+import { UserEnum } from './shared/user.model';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +10,25 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent implements OnInit {
   title = 'angular-services';
-  constructor() {}
+
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.userService.getAll().subscribe((users) => console.log(users));
+
+    this.userService.getById(0).subscribe((user) => console.log(user));
+
+    this.userService.getByUsername('adrian').subscribe((users) => console.log(users));
+
+    this.userService.saveUser({
+      age: 60,
+      username: 'Peppe',
+      company: 'ilegra',
+      nationality: 'Russian',
+      role: UserEnum.USER
+    })
+    //.subscribe();
+
+    this.userService.getByUsername('Peppe').subscribe((users) => console.log(users));
   }
 }
---
+++ packages/3 - angular-services/src/app/services/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { User } from '../shared/user.model';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(`${environment.apiUrl}/users`);
+  }
+
+  getById(id: number): Observable<User> {
+    return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
+  }
+
+  getByUsername(username: string): Observable<User[]> {
+    const params = new HttpParams().append('username', username);
+
+    return this.http.get<User[]>(`${environment.apiUrl}/users`, {params});
+  }
+
+  saveUser(user: User): Observable<any> {
+    return this.http.post<void>(`${environment.apiUrl}/users`, user);
+  }
+}
